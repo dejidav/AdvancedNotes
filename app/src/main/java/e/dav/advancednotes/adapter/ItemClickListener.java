fd04e32c28d84993bfdbdd69a6813b725937b0eb
dejidav <dejidav04@gmail.com>
2018-11-23 11:52:14 -0500

    new updated project

---
+++ app/src/main/java/e/dav/advancednotes/adapter/ItemClickListener.java
@@ -0,0 +1,10 @@
+package e.dav.advancednotes.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+
+public interface ItemClickListener {
+    void onItemClicked(RecyclerView.ViewHolder vh, Object item, int pos);
+}
+
+
